5e17faaf0015dfd3d924e041074b722e1efad1db
Schade9 <s.charlesderrick9@gmail.com>
2023-03-13 16:24:40 +0300

    desktop view

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import image from './assets/illustration-hero.svg';
+import logo from './assets/icon-music.svg';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <section>
+        <div className="image">
+          <img src={image} alt="" />
+        </div>
+        <div className="order-summary">
+          <h1>Order Summary</h1>
+          <p className='description'>You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!</p>
+          <div className="annual-plan">
+            <div className='detail'>
+              <img src={logo} alt="" />
+              <div>
+                <h3>Annual Plan</h3>
+                <p>$59.99/year</p>
+              </div>
+            </div>
+            <div>
+              <a href="#">Change</a>
+            </div>
+          </div>
+          <div className="buttons">
+            <button className='proceed'>Proceed to Payment</button>
+            <button className='cancel'>Cancel Order</button>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
